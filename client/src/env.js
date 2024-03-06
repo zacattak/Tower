408034928cbbbc6c0663226f800fa6c97158e5ae
Zac <step7476@alumni.uidaho.edu>
2024-03-06 13:35:53 -0700

    setup

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-pegy5uc57khuj5zb.us.auth0.com'
+export const clientId = 'D3Z921gUcOqLsTwDRJQVd8suMTIm8BFs'
+export const audience = 'zacapi'

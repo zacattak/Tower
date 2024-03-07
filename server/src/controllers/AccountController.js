4a7409b681027eec99574bb7fab430310efca490
Zac <step7476@alumni.uidaho.edu>
2024-03-07 16:43:55 -0700

    save

--- server/src/controllers/AccountController.js
+++ server/src/controllers/AccountController.js
@@ -1,6 +1,7 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
 import BaseController from '../utils/BaseController'
+import { ticketsService } from '../services/TicketsService.js'
 
 export class AccountController extends BaseController {
   constructor() {
@@ -9,6 +10,7 @@ export class AccountController extends BaseController {
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
       .put('', this.editUserAccount)
+      .get('/tickets', this.getMyTickets)
   }
 
   async getUserAccount(req, res, next) {
@@ -20,7 +22,7 @@ export class AccountController extends BaseController {
     }
   }
 
-   async editUserAccount(req, res, next) {
+  async editUserAccount(req, res, next) {
     try {
       const accountId = req.userInfo.id
       req.body.id = accountId
@@ -30,5 +32,15 @@ export class AccountController extends BaseController {
       next(error)
     }
   }
-  
+
+  async getMyTickets(request, response, next) {
+    try {
+      const userId = request.userInfo.id
+      const myTickets = await ticketsService.getEventTickets(userId)
+      response.send(myTickets)
+    } catch (error) {
+      next(error)
+    }
+  }
+
 }
